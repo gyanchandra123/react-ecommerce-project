2232a29e7f6a3827761f9dc041170eb55521b9a4
gyanchandra moirangthem <gyanchandra moirangthem>
2022-01-02 21:27:33 +0530

    updating app with the header section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
- 
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="shop" element={<ShopPage />} />
@@ -17,4 +18,3 @@ function App() {
 }
 
 export default App;
-  
